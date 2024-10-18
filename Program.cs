d2eeaa4867c5d628a0754f53b8ca0b1a7cac24d2
Levente <gecsel@kkszki.hu>
2024-10-18 11:11:28 +0200

    Jelszo project is created

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga_2
+{
+    internal class Program
+    {
+        public static void Jelszo()
+        {
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Jelszo();
+        }
+    }
+}
